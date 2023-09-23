5d39e278c6ab969a4ce17f8d6bafebfbb59ff33d
qfendor <145855345+qfendor@users.noreply.github.com>
2023-09-23 12:16:58 -0500

    puts mf on the test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TEST!!! <code>src/App.js</code> and save to load.
+          TEST!!! mf <code>src/App.js</code> and save to load.
         </p>
         <a
           className="App-link"
