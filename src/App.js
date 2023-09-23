a4915a35c208c7889728270d5defdc5311b37e05
qfendor <145855345+qfendor@users.noreply.github.com>
2023-09-23 14:10:19 -0500

    delete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-<<<<<<< HEAD
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 
 
 
 
-=======
->>>>>>> ca8e865ad422733eef1773118c73fe539a817d41
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
-          TEST!! mf <code>src/App.js</code> and save to load.
-          
-=======
           TEST!!! mf <code>src/App.js</code> and save to load.
           This is more of a test!
 >>>>>>> ca8e865ad422733eef1773118c73fe539a817d41
