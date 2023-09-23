a688a36536e621b541396d822533ad172af84021
ahmedgaddal <145858189+ahmedgaddal@users.noreply.github.com>
2023-09-23 13:57:44 -0500

    took google map out

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from './downloads/york.png';
+import logo from './logo.svg';
 import './App.css';
 
+<<<<<<< HEAD
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+
+
+
+
+=======
+>>>>>>> ca8e865ad422733eef1773118c73fe539a817d41
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+<<<<<<< HEAD
+          TEST!! mf <code>src/App.js</code> and save to load.
+          
+=======
           TEST!!! mf <code>src/App.js</code> and save to load.
           This is more of a test!
+>>>>>>> ca8e865ad422733eef1773118c73fe539a817d41
         </p>
         <a
           className="App-link"
